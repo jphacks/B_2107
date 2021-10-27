ea3ba77d1698095e24aaa31660ce27ceecceb9c9
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2021-10-28 00:07:13 +0900

    add-update-agenda
    議題を変更できるようにしました

--- web/src/views/Meet.vue
+++ web/src/views/Meet.vue
@@ -5,12 +5,7 @@
       shrink-on-scroll
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-       <v-col
-            v-for="(data,index) in agenda"
-            :key="index"
-          >
-      <v-toolbar-title>{{data.agenda}}</v-toolbar-title>
-       </v-col>
+      <v-toolbar-title class="title"></v-toolbar-title>
       <v-spacer></v-spacer>
 
       <v-btn icon>
@@ -19,6 +14,87 @@
     </v-app-bar>
 
     <v-main>
+      <v-col
+            v-for="(data,index) in agenda"
+            :key="index"
+          >
+       <v-card
+    class="mx-auto agenda"
+    color="green"
+    dark
+    max-width="800"
+  >
+    <v-card-title >
+          <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+     <v-icon
+  color="white"
+  x-large
+  v-bind="attrs"
+  v-on="on"
+>mdi-plus</v-icon>
+      </template>
+         <v-card>
+        <v-card-title>
+          <span class="text-h5">議題変更</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                v-model="body"
+                  label="議題を入力してください"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog_f"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog_t"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    </v-card-title>
+      <v-row
+          align="center"
+          justify="end"
+        >
+    <v-card-text class="text-h3 font-weight-bold">
+      {{data.agenda}}
+    
+    </v-card-text>
+      </v-row>
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-row
+          align="center"
+          justify="center"
+        >
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+  </v-card>
+      </v-col>
       <v-container>
         <v-row>
           <v-col
@@ -28,12 +104,12 @@
           >
              <v-card
     class="mx-auto"
-    color="#26c6da"
+    color="green"
     dark
     max-width="300"
   >
     <v-card-title>
-      <span class="text-h7 font-weight-light">opinion</span>
+      <span class="opacity">a</span>
     </v-card-title>
     <v-card-text class="text-h5 font-weight-bold">
                          {{data.opinion}}
@@ -70,6 +146,7 @@
       <v-col>
 
 <v-btn
+  class="vote-btn"
   color="accent"
   depressed
   elevation="10"
@@ -127,6 +204,7 @@ import firebase from "@/firebase/firebase.js";
       if (change.type === "added") {
         // 保存済みのデータ n 件分出力される
        this.agenda.push(change.doc.data())
+       this.id.push(change.doc.id)
       }
        else if(change.type === 'modified' ){
        this.agenda.splice(0, 1, change.doc.data())
@@ -144,6 +222,7 @@ import firebase from "@/firebase/firebase.js";
 
 },
     data: () => ({
+      dialog: false,
       opinions: [
       ],
       agenda: [
@@ -156,7 +235,10 @@ import firebase from "@/firebase/firebase.js";
       ],
       votes:[
 
-      ]
+      ],
+      id:[
+
+      ],
     }),
     computed: {
 
@@ -166,16 +248,39 @@ import firebase from "@/firebase/firebase.js";
         var str = this.votes.join(',');
         this.$router.push({name:"Result", params: {id: this.$route.params.id, password: this.$route.params.password, re : str}}).catch(() => {});
          window.location.reload();
-      }
+      },
+      dialog_f(){
+        this.dialog=false
+      },
+      async dialog_t(){
+        this.dialog=false;
+  const db = firebase.firestore();
+const userRef = db.collection('mtg').doc(this.id[0])
+await userRef.update({
+  agenda: this.body,
+});
+      },
     }
   }
 </script>
 <style scoped>
-.v-btn{
-position: absolute;
-bottom: 10px;
-margin-left: auto;
-margin-right: auto;
+.vote-btn{
+position: center;
+
 }
+.opacity{
+  opacity: 0%;
+}
+.title{
+position: absolute;
+margin-bottom: 50%;
+margin-left: 40%;
+margin-right: 50%;
+}
+.agenda{
+  width:auto;
+  height: auto;
+}
+
 </style>
 
